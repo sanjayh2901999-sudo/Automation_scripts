5e6aa8acfd9e673bc4446ad4d0d0382d96c1204f
sanjayh2901999-sudo <sanjayh2901999@gmail.com>
2026-05-04 15:16:26 +0530

    product class is Added new pom class

---
+++ Com_Cast_CRM_GUI_Framework_Advance_Selenium_Tek_Pyramid_Duplicate/src/main/java/Com/ComCast/CRM/Object_Repository_Utility/Products.java
@@ -0,0 +1,10 @@
+package Com.ComCast.CRM.Object_Repository_Utility;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Products
+{
+@FindBy(xpath="//input[@alt='Create Product...']")
+private WebElement createProductImgBtn;
+}
